9aecf12ca253a6cbc30209e8667fa1627a0eda8a
402-25-Pimchanok <54384@yupparaj.ac.th>
2026-08-19 15:34:57 +0700

    Update vocap_game.py

--- vocap_game.py
+++ vocap_game.py
@@ -100,11 +100,11 @@ ans2 = st.text_input(
     "ข้อ 2: Cats love to eat `f _ s h`. 🐟",
     value=st.session_state.ans2_val,
 )
-ans3 = st.text_input
+ans3 = st.text_input(
     "ข้อ 3: It uses ink to write `_ e n`. 🖊️",
     value=st.session_state.ans3_val,
 )
-ans4 = st.text_input
+ans4 = st.text_input(
     "ข้อ 4: It has numbers to measure length or centimeters `R u _ e _`. 📏",
     value=st.session_state.ans4_val,
 
