0784c128d6798a4c554ddd9925d5273a22c24773
402-25-Pimchanok <54384@yupparaj.ac.th>
2026-08-19 15:05:28 +0700

    Update vocap_game.py

--- vocap_game.py
+++ vocap_game.py
@@ -11,7 +11,7 @@ if "ans2_val" not in st.session_state:
 
 
 # 📌 ฟังก์ชันเคลียร์ค่าเมื่อกดปุ่มเริ่มใหม่
-  def reset_game():
+def reset_game():
     st.session_state.ans1_val = ""  # เคลียร์ค่าช่องข้อ 1
     st.session_state.ans2_val = ""  # เคลียร์ค่าช่องข้อ 2
     st.session_state.start = time.time()  # เริ่มเวลาใหม่
