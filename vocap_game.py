e486fda1f0f9aa24f7404f23cfd4ef183dcfd0c6
402-25-Pimchanok <54384@yupparaj.ac.th>
2026-08-19 15:06:39 +0700

    Update vocap_game.py

--- vocap_game.py
+++ vocap_game.py
@@ -26,7 +26,7 @@ def show_result_dialog(ans1, ans2):
     st.balloons()
     score = 0
 
- u_ans1 = ans1.strip().lower()
+    u_ans1 = ans1.strip().lower()
     u_ans2 = ans2.strip().lower()
 
  # ตรวจข้อ 1
